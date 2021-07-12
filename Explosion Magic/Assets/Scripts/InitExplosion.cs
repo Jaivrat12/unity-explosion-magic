86f6168caf432efa48814b19cd3ade10bfe07a55
Jaivrat Das <jaivratdas123456@gmail.com>
2021-07-12 14:43:42 +0530

    Project Completed

---
+++ Explosion Magic/Assets/Scripts/InitExplosion.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitExplosion : MonoBehaviour {
+
+    public Vector3 location;
+    public GameObject[] detonators;
+    public float[] timeIntervals;
+
+    private int objects, i = 0;
+
+
+    // Start is called before the first frame update
+    void Start() {
+
+        objects = detonators.Length;
+        location = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if(i < objects && Time.time >= timeIntervals[i]) {
+
+            //Detonator dTemp = (Detonator)currentDetonator.GetComponent("Detonator");
+
+            //float offsetSize = dTemp.size/3;
+            //Vector3 hitPoint = hit.point +
+            //                          ((Vector3.Scale(hit.normal, new Vector3(offsetSize, offsetSize, offsetSize))));
+            GameObject exp = (GameObject) Instantiate(detonators[i], location, Quaternion.identity);
+            //dTemp = (Detonator)exp.GetComponent("Detonator");
+            //dTemp.detail = detailLevel;
+
+            //Destroy(exp, explosionLife);
+            ++i;
+        }
+    }
+}
---
+++ Explosion Magic/Assets/Scripts/dimLights.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dimLights : MonoBehaviour {
+
+    private bool rotate = true;
+    private bool stage1 = true;
+    public float rateX;
+
+    // Start is called before the first frame update
+    void Start() {
+
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if(rotate) {
+
+            Debug.Log(transform.eulerAngles);
+            if(stage1 && transform.eulerAngles.x > 0) {
+
+                transform.Rotate(rateX, 0, 0);
+            }
+            else if(stage1) {
+
+                stage1 = false;
+                transform.Rotate(rateX, 0, 0);
+                rateX /= 5;
+            }
+            else if(transform.eulerAngles.x > 353) {
+                
+                transform.Rotate(rateX, 0, 0);
+            }
+            else
+                rotate = false;
+        }
+    }
+}
